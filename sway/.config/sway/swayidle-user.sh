495ae03c039cd6447fb8b209a17d002300359f46
Seth Barberee <seth.barberee@gmail.com>
2020-09-06 20:27:00 -0500

    update sway files

--- sway/.config/sway/swayidle-user.sh
+++ sway/.config/sway/swayidle-user.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-. "/home/seth/.cache/wal/colors.sh"
+$HOME/.cache/wal/colors.sh
 
 # TODO test if swayidle and swaylock exits
 
 swayidle -w \
-    timeout 300 'swaylock -i /home/seth/wallpapers/iceburg.jpg' \
+    timeout 300 'swaylock -i $wallpaper' \
     timeout 600 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' \
-    before-sleep 'swaylock -i /home/seth/wallpapers/iceburg.jpg'
+    before-sleep 'swaylock -i $wallpaper'
