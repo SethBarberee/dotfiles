0654704782b77c6f5bab167d959013bda37a8b84
Seth Barberee <seth.barberee@gmail.com>
2023-04-23 16:21:07 -0700

    nvim: fix up getPoryscriptFiles handler

--- neovim/.config/nvim/lua/seth/plugins/lsp/handlers.lua
+++ neovim/.config/nvim/lua/seth/plugins/lsp/handlers.lua
@@ -8,44 +8,54 @@ local handlers = {
 }
 
 -- Poryscript needs custom handlers so we do that here
-if filetype == "pory" then
-    handlers["poryscript/getPoryscriptFiles"] = function(_, result, ctx, _)
-        local base_dir = vim.fn.getcwd()
-        -- TODO: search for all *pory files for this request
-        local files = vim.fn.expand(base_dir .. "/data/scripts/test2.pory")
-        print(files)
-        local files_array = { files }
-        return files_array
+--if filetype == "pory" then
+handlers["poryscript/getPoryscriptFiles"] = function(_, result, ctx, _)
+    -- search for all *pory files for this request
+    -- and return a table of URIs
+
+    -- get all files ending with .pory
+    local files = vim.fs.find(function(name, path)
+        return name:match('.*%.pory$')
+    end, { limit = math.huge, type = 'file' })
+
+    --print(vim.inspect(files))
+
+    local files_array = {}
+    for k, v in pairs(files) do
+        local uri_file = vim.uri_from_fname(v)
+        table.insert(files_array, uri_file)
     end
+    print(vim.inspect(files_array))
+    return files_array
+end
 
-    handlers["poryscript/getfileuri"] = function(_, result, ctx, _)
-        print("get file uri")
-        print(vim.inspect(result))
-        return vim.uri_from_bufnr(0)
-    end
+handlers["poryscript/getfileuri"] = function(_, result, ctx, _)
+    print("get file uri")
+    print(vim.inspect(result))
+    return vim.uri_from_bufnr(0)
+end
 
-    handlers["poryscript/readfile"] = function(_, result, ctx, _)
-        local bufnr = vim.uri_to_bufnr(result)
-        if vim.api.nvim_buf_is_loaded(bufnr) then
-            -- NOTE: Read the lines in the buffer and then combine into a string since poryscript-pls expects a string
-            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-            return table.concat(lines, "\n")
-        else
-            return ""
-        end
-    end
-
-    handlers["poryscript/readfs"] = function(_, result, ctx, _)
-        local bufnr = vim.uri_to_bufnr(result)
-        if vim.api.nvim_buf_is_loaded(bufnr) then
-            -- NOTE: Read the lines in the buffer and then combine into a string since poryscript-pls expects a string
-            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-            return table.concat(lines, "\n")
-        else
-            return ""
-        end
-
+handlers["poryscript/readfile"] = function(_, result, ctx, _)
+    local bufnr = vim.uri_to_bufnr(result)
+    if vim.api.nvim_buf_is_loaded(bufnr) then
+        -- NOTE: Read the lines in the buffer and then combine into a string since poryscript-pls expects a string
+        local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+        return table.concat(lines, "\n")
+    else
+        return ""
     end
 end
+
+handlers["poryscript/readfs"] = function(_, result, ctx, _)
+    local bufnr = vim.uri_to_bufnr(result)
+    if vim.api.nvim_buf_is_loaded(bufnr) then
+        -- NOTE: Read the lines in the buffer and then combine into a string since poryscript-pls expects a string
+        local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+        return table.concat(lines, "\n")
+    else
+        return ""
+    end
+end
+--end
 
 return handlers
