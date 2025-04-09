fd6a039d0c9a80f3b9ae1ce3ca72a19601773af1
Seth Barberee <seth.barberee@gmail.com>
2025-04-08 21:25:14 -0700

    nvim: update fidget

--- neovim/.config/nvim/lua/seth/plugins/fidget.lua
+++ neovim/.config/nvim/lua/seth/plugins/fidget.lua
@@ -1,6 +1,6 @@
 -- Use legacy tag for fidget until rewrite is done
 return {
     "j-hui/fidget.nvim",
-    tag = "legacy",
-    opts = { sources = { ["null-ls"] = { ignore = true } } }
+    version = "v1.*",
+    opts = {}
 } -- Lsp status notifications
