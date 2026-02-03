b2732059888ec461de4f1bec60b3dd256ee5559f
Seth Barberee <seth.barberee@gmail.com>
2026-02-02 20:10:53 -0800

    nvim: update nvim config for treesitter changes

--- neovim/.config/nvim/lua/seth/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/seth/plugins/treesitter.lua
@@ -15,7 +15,7 @@ return {
             -- Luckily, the only thins that those plugins need are the custom queries, which we make available
             -- during startup.
             require("lazy.core.loader").add_to_rtp(plugin)
-            require("nvim-treesitter.query_predicates")
+            --require("nvim-treesitter.query_predicates")
         end,
         dependencies = {
             'nvim-treesitter/nvim-treesitter-textobjects',
@@ -57,70 +57,36 @@ return {
             },
         },
         config = function()
-            require('nvim-treesitter.configs').setup {
-                -- Make sure these are installed
-                ensure_installed = { "c", "lua", "rust", "query", "javascript", "bash", "make" },
-                highlight = {
-                    enable = true, -- false will disable the whole extension
-                    -- additional_vim_regex_highlighting = "vim",
-                    -- Seems like treesitter is a lot better now so let's enable it for every c file again
-                    --disable = function(lang, bufnr) -- Disable in large C buffers
-                    --    return lang == "c" and vim.api.nvim_buf_line_count(bufnr) > 8000
-                    --end,
-                },
-                indent = {
-                    enable = true,
-                },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["ac"] = "@conditional.outer",
-                            ["ic"] = "@conditional.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["al"] = "@loop.outer",
-                            ["il"] = "@loop.inner",
-                            ["av"] = "@parameter.outer",
-                            ["iv"] = "@parameter.inner",
+            require('nvim-treesitter').install({ 'c', 'lua', 'rust', 'query', 'bash', 'make' }):wait(300000) -- wait max. 5 minutes
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = { 'c', 'lua', 'rust', 'query', 'bash', 'make', 'arm' },
+                callback = function() vim.treesitter.start() end,
+            })
+
+            vim.api.nvim_create_autocmd('User', {
+                pattern = { 'TSUpdate' },
+                callback = function()
+                    require('nvim-treesitter.parsers').arm = {
+                        install_info = {
+                            path = "~/tree-sitter-asm", -- local path or git repo
+                            -- optional entries:
+                            --branch = "main", -- default branch in case of git repo if different from master
+                            generate = true, -- if folder contains pre-generated src/parser.c
                         },
-                        -- You can choose the select mode (default is charwise 'v')
-                        selection_modes = {
-                            ['@parameter.outer'] = 'v', -- charwise
-                            ['@function.outer'] = 'V',  -- linewise
-                            ['@class.outer'] = '<c-v>', -- blockwise
+                    }
+                    require('nvim-treesitter.parsers').poryscript = {
+                        install_info = {
+                            url = "https://github.com/Elsie19/treesitter-poryscript",
+                            -- optional entries:
+                            --branch = "main", -- default branch in case of git repo if different from master
+                            generate = true, -- if folder contains pre-generated src/parser.c
                         },
-                    },
-                },
-            }
-            if sethconfig.treesitter_extras then
-                local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-                parser_config.arm = {
-                    install_info = {
-                        url = "~/tree-sitter-asm", -- local path or git repo
-                        files = { "src/parser.c" },
-                        -- optional entries:
-                        --branch = "main", -- default branch in case of git repo if different from master
-                        generate_requires_npm = false,         -- if stand-alone parser without npm dependencies
-                        requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
-                    },
-                    filetype = "arm",                          -- if filetype does not match the parser name
-                }
-                parser_config.poryscript = {
-                    install_info = {
-                        url = "https://github.com/Elsie19/treesitter-poryscript",
-                        files = { "src/parser.c" },
-                        -- optional entries:
-                        -- branch = "main",                       -- default branch in case of git repo if different from master
-                        generate_requires_npm = false,         -- if stand-alone parser without npm dependencies
-                        requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
-                    },
-                    filetype = "pory",                         -- if filetype does not match the parser name
-                }
-            end
-        end,
-    },
+                    }
+                end
+            })
+
+            vim.treesitter.language.register('arm', { 'arm' })
+            vim.treesitter.language.register('poryscript', { 'pory' })
+        end
+    }
 }
